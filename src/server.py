8ed3cc00cdc056a604e8e2b25ab6c3b30d4f916b
Smruti Kshirsagar <s.s.kshirsagar@student.tudelft.nl>
2024-05-21 19:38:20 +0200

    Pylint fix

--- src/server.py
+++ src/server.py
@@ -9,10 +9,8 @@ from libversion import version_util
 lv = version_util.VersionUtil()
 ver = lv.get_version()
 
-count_idx = 0
-count_pred = 0
-count_pred_phish = 0
-count_pred_legit = 0
+# pylint: disable-next=global-statement
+COUNT_IDX, COUNT_PRED = 0, 0
 
 # get environment variables from docker ran command
 model_url = os.getenv('MODEL_URL', 'http://localhost:5000/process_link')
@@ -29,8 +27,9 @@ def index():
     Load template page and send it to the user.
     Returns: The default web template.
     '''
-    global count_idx
-    count_idx += 1
+    # pylint: disable-next=global-statement
+    global COUNT_IDX
+    COUNT_IDX += 1
     return render_template(
         "index.html",
         inputDisplay="",
@@ -45,8 +44,9 @@ def predict():
     Define the /predict route.
     Returns: The results web template.
     '''
-    global count_pred
-    count_pred += 1
+    # pylint: disable-next=global-statement
+    global COUNT_PRED
+    COUNT_PRED += 1
     # Get the link from the html page and send it to the service
     url = request.args.get("url")
     data = {"url": url}
@@ -61,51 +61,44 @@ def predict():
     )
 
 
-@app.route('/url_was_phising')
-def url_was_phising():
-    '''
-    Define the /url_was_phising route.
-    Returns: The phising web template.
-    '''
-    global count_pred_phish
-    count_pred_phish += 1
-    return
+# @app.route('/url_was_phising')
+# def url_was_phising():
+#     '''
+#     Define the /url_was_phising route.
+#     Returns: The phising web template.
+#     '''
+#     return
 
 
-@app.route('/url_was_legit')
-def url_was_legit():
-    '''
-    Define the /url_was_legit route.
-    Returns: The legit web template.
-    '''
-    global count_pred_legit
-    count_pred_legit += 1
-    return
+# @app.route('/url_was_legit')
+# def url_was_legit():
+#     '''
+#     Define the /url_was_legit route.
+#     Returns: The legit web template.
+#     '''
+#     return
 
 
 @app.route('/metrics')
 def show_site_metrics():
-    global count_idx, count_pred, count_pred_phish, count_pred_legit
-    m = ""
-    m += "# HELP num_requests The number of requests that \
+    '''
+    Define the /metric route.
+    Returns: The metrics web template.
+    '''
+    metric_str = ""
+    metric_str += "# HELP num_requests The number of requests that \
             have been served, by page.\n"
-    m += "# TYPE num_requests counter\n"
-    m += "num_requests{{page=\"index\"}} {}\n".format(count_idx)
-    m += "num_requests{{page=\"predict\"}} {}\n\n"\
-        .format(count_pred)
-    # m += "num_requests{{page=\"url_was_phising\"}} {}\n\n"\
-    #         .format(count_pred_phish)
-    # m += "num_requests{{page=\"url_was_legit\"}} {}\n\n"\
-    #         .format(count_pred_legit)
+    metric_str += "# TYPE num_requests counter\n"
+    metric_str += f"num_requests{{page=\"index\"}} {COUNT_IDX}\n"
+    metric_str += f"num_requests{{page=\"predict\"}} {COUNT_PRED}\n\n"
 
-    m += "# HELP index_relevance The percentage of requests \
+    metric_str += "# HELP index_relevance The percentage of requests \
          that are served by index.\n"
-    m += "# TYPE index_relevance gauge\n"
-    m += "index_relevance {}\n".format(min(1.0, float(count_idx))
-                                       if count_idx == 0
-                                       else count_idx/count_pred)
+    metric_str += "# TYPE index_relevance gauge\n"
+    idx_rel = min(1.0, float(COUNT_IDX) if COUNT_IDX == 0 else COUNT_IDX/COUNT_PRED)
+    metric_str += f"index_relevance {idx_rel}"
 
-    return Response(m, mimetype="text/plain")
+    return Response(metric_str, mimetype="text/plain")
 
 
 if __name__ == '__main__':
